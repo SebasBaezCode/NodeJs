da5f0fa2e375f020b64677d8a3501066deb27e46
SebastianBaezCode <jsbaez5@misena.edu.co>
2018-09-10 22:12:03 -0500

    Código listo para ser testeado por primera vez con POSTMAN

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+'use scrict'
+
+const services = require('../services')
+
+function isAuth(req, res, next){
+	if (!req.headers.authorization) return res.status(403).send({ message: `No tienes autorización`})
+
+	const token = req.headers.authorization.split("  ")[1]
+	req.user = payload.sub
+
+	services.decodeToken(token)
+		.then(response => {
+			req.user = response
+			next()
+		})
+		.catch(response => {
+			res.status(response.status)
+		})
+}
+
+
+module.exports = isAuth
+
